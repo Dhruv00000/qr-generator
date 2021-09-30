8ee42395f4bce1b0ef5f3b793c12d67eae434d8c
DhruvMitna19 <dhruvmitna9@gmail.com>
2021-09-30 11:09:43 +0530

    added comments

--- qr.py
+++ qr.py
@@ -1,17 +1,21 @@
+# Importing the necessary functions from modules.
 from qrcode import QRCode
 from qrcode.constants import ERROR_CORRECT_H
 
 try:
 
+    # Getting the user input.
     Data = input("Enter the text or link that you want to show: ")
     size = float(input("Enter the size of the qrcode (default = 15): "))
     borderSize = float(input("Enter the size of the border of the qrcode: "))
     Fit = input("Enter yes if you want the qrcode size to be responsive: ").lower()
 
+# If the user enters a string where an integer was required, this message is printed out instead of the program crashing.
 except ValueError:
 
     print("Please enter a valid value.")
 
+# Generating the qrcode.
 qr = QRCode(error_correction = ERROR_CORRECT_H, box_size = size, border = borderSize)
 qr.add_data(Data)
 
@@ -32,8 +36,10 @@ try:
 
     fileName = input("Enter the name of the image file of the qrcode: ").lower()
 
+    # saving the image.
     img.save(f"output/{fileName}.png")
 
-except ValueError:
+# If the user enters an unsupported colour, this message is printed out instead of the program crashing.
+except (ValueError, NameError) :
 
     print("Colour not supported.")
